7ca3e9ca40de8b7d8c93067e251756e31489669c
Muhammad Arslan <arslanashiq@beyonderissolutions.com>
2024-01-06 15:00:03 +0500

    supplier statement

--- src/styles/mui/container/accounting/purchase/supplier/detail/components/supplier-statement.js
+++ src/styles/mui/container/accounting/purchase/supplier/detail/components/supplier-statement.js
@@ -1,13 +1,11 @@
 export const supplierStatementHeaderCellStyles = {
   padding: '5px 10px 5px 10px',
   wordWrap: 'break-word',
-  border: '0.2px solid lightgray',
   fontSize: 14,
 };
 export const supplierStatementHeaderRowStyles = { height: 32 };
 export const supplierStatementBodyRowStyles = {
   padding: '10px 10px 10px 10px',
-  border: '0.2px solid lightgray',
   backgroundColor: '#f7f4f4b6',
 };
 export const supplierStatementBodyCellStyles = { wordWrap: 'break-word' };
