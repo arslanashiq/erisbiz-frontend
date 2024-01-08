df35139484a67e30463b9acf51c7a58cf2eaab49
Muhammad Arslan <arslanashiq@beyonderissolutions.com>
2024-01-08 16:29:02 +0500

    sorted journal for sale

--- src/utilities/get-sorted-journals.js
+++ src/utilities/get-sorted-journals.js
@@ -1,18 +1,22 @@
 export const handleGetSortedData = (journalObject, sortedList) => {
-  const journalObjects = { ...journalObject };
   const sortedJournals = [];
-  sortedList.forEach((COA, index) => {
-    if (journalObject[COA]) {
-      sortedJournals.splice(index, 0, { ...journalObjects[COA] });
-      delete journalObjects[COA];
-    }
-  });
-  const otherAccountsIndex = sortedList.indexOf('otherAccounts');
-  Object.keys(journalObjects).forEach(journal => {
-    sortedJournals.splice(otherAccountsIndex, 0, journalObjects[journal]);
-  });
+  try {
+    const journalObjects = { ...journalObject };
+    sortedList.forEach((COA, index) => {
+      if (journalObject[COA]) {
+        sortedJournals.splice(index, 0, { ...journalObjects[COA] });
+        delete journalObjects[COA];
+      }
+    });
+    const otherAccountsIndex = sortedList.indexOf('otherAccounts');
+    Object.keys(journalObjects).forEach(journal => {
+      sortedJournals.splice(otherAccountsIndex, 0, journalObjects[journal]);
+    });
 
-  return sortedJournals;
+    return sortedJournals;
+  } catch (error) {
+    return sortedJournals;
+  }
 };
 
 export const test = '';
