e72288c6eeb2a57882827af18af93ff4e0005ca0
Muhammad Arslan <arslanashiq@beyonderissolutions.com>
2024-01-16 19:15:15 +0500

    apagingDetail

--- src/containers/reports/payables/ApAgingDetail.jsx
+++ src/containers/reports/payables/ApAgingDetail.jsx
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 // services
 import { useGetApAgingDetailQuery } from 'services/private/reports';
+import { useGetSuppliersListQuery } from 'services/private/suppliers';
 
+import useListOptions from 'custom-hooks/useListOptions';
 import { apAgingDetailReportHeadCells } from 'containers/reports/utilities/head-cells';
 import useGetApAgingDetailData from 'containers/reports/custom-hooks/payables/useGetApAgingDetailData';
 // components
-import CustomReportDetailPage from '../components/CustomReportDetailPage';
 // styles
+import { AgingByList, FilterReportsList } from '../utilities/constants';
+import CustomReportDetailPage from '../components/CustomReportDetailPage';
 import 'styles/reports/reports.scss';
 
 function ApAgingDetail() {
+  const supplierApiResponse = useGetSuppliersListQuery();
+
+  const { optionsList: suppliersOptions } = useListOptions(supplierApiResponse?.data?.results, {
+    value: 'id',
+    label: 'supplier_name',
+  });
+  const updatedPayablesSupplierBalanceCustomInputList = useMemo(() => {
+    const newinputList = [];
+
+    newinputList.push({
+      label: 'Duration',
+      name: 'duration',
+      labelClassName: '',
+      className: 'w-100',
+      options: FilterReportsList,
+      fullWidth: true,
+    });
+
+    newinputList.push({
+      label: 'Supplier',
+      options: suppliersOptions || [],
+      name: 'supplier_id',
+      labelClassName: '',
+      className: 'w-100',
+      fullWidth: true,
+    });
+    newinputList.push({
+      label: 'Aging By',
+      name: 'date_type',
+      labelClassName: '',
+      className: 'w-100',
+      options: AgingByList,
+      fullWidth: true,
+    });
+    return newinputList;
+  }, [suppliersOptions, FilterReportsList]);
   return (
     <CustomReportDetailPage
       reportTitle="Ap Aging Detail"
       reportHeadCells={apAgingDetailReportHeadCells}
       useGetReportQuery={useGetApAgingDetailQuery}
       useGetReportData={useGetApAgingDetailData}
+      customReportCustomFilter={updatedPayablesSupplierBalanceCustomInputList}
+      customReportCustomerInitialValues={{}}
     />
   );
 }
