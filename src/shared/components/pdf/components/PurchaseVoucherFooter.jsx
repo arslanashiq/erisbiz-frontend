97ddb11419e605c601ff50f707af11428f0984e7
Muhammad Arslan <arslanashiq@beyonderissolutions.com>
2024-01-23 15:59:35 +0500

    payment-voucher print

--- src/shared/components/pdf/components/PurchaseVoucherFooter.jsx
+++ src/shared/components/pdf/components/PurchaseVoucherFooter.jsx
@@ -3,49 +3,40 @@ import moment from 'moment';
 import { v4 as uuid } from 'uuid';
 import { Text, View } from '@react-pdf/renderer';
 import formatAmount from 'utilities/formatAmount';
-import { DATE_FORMAT, supplierOpeningBalanceName } from 'utilities/constants';
+import { DATE_FORMAT } from 'utilities/constants';
 
 function PurchaseVoucherFooter({ orderDetail, keyName, styles }) {
+  const getName = item => {
+    if (item?.bill) {
+      return item?.bill?.bill_num;
+    }
+    return 'Opening Balance';
+  };
   return (
     orderDetail[keyName] &&
     orderDetail[keyName].map(item => (
       <View key={uuid()} style={styles.tableRow}>
         <View style={styles.tableCol}>
-          {item.invoice ? (
-            <Text style={[styles.tableCell, { textAlign: 'center' }]}>{item.invoice.invoice_id}</Text>
-          ) : (
-            <Text style={[styles.tableCell, { textAlign: 'center' }]}>{supplierOpeningBalanceName}</Text>
-          )}
+          <Text style={[styles.tableCell, { textAlign: 'center' }]}>
+            {moment(item?.bill?.bill_date || item.created_at).format(DATE_FORMAT)}
+          </Text>
         </View>
         <View style={styles.tableCol}>
-          {item.bill && (
-            <Text style={[styles.tableCell, { textAlign: 'center' }]}>
-              {moment(item.bill.bill_date).format(DATE_FORMAT)}
-            </Text>
-          )}
-          {item.supplier && (
-            <Text style={[styles.tableCell, { textAlign: 'center' }]}>
-              {moment(item.supplier.bill_date).format(DATE_FORMAT)}
-            </Text>
-          )}
+          <Text style={[styles.tableCell, { textAlign: 'center' }]}>{getName(item)}</Text>
         </View>
         <View style={styles.tableCol}>
-          {item.bill && (
-            <Text style={[styles.tableCell, { textAlign: 'center' }]}>
-              {orderDetail.currency_symbol}
-              {formatAmount(item.bill.grand_total)}
-            </Text>
-          )}
-          {item.supplier && (
-            <Text style={[styles.tableCell, { textAlign: 'center' }]}>
-              {orderDetail.currency_symbol}
-              {formatAmount(item.supplier.grand_total)}
-            </Text>
-          )}
+          <Text style={[styles.tableCell, { textAlign: 'center' }]}>{item?.bill?.pur_order_num || '-'}</Text>
         </View>
         <View style={styles.tableCol}>
           <Text style={[styles.tableCell, { textAlign: 'center' }]}>
-            {moment(item.created_at).format(DATE_FORMAT)}
+            {orderDetail.currency_symbol}
+            {formatAmount(item?.bill?.grand_total || item.supplier?.grand_total)}
+          </Text>
+        </View>
+        <View style={styles.tableCol}>
+          <Text style={[styles.tableCell, { textAlign: 'center' }]}>
+            {orderDetail.currency_symbol}
+            {formatAmount(item?.bill?.amount_due || item.supplier?.amount_due)}
           </Text>
         </View>
         <View style={styles.tableCol}>
