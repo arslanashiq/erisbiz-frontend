5b26fb3565cc51b2e0f150fb500895a410d00f47
Arslan Ashiq <arslanashiq@beyonderissolutions.com>
2024-02-06 19:36:38 +0500

    general ledger filters

--- src/containers/reports/accountant/GeneralLedger.jsx
+++ src/containers/reports/accountant/GeneralLedger.jsx
@@ -3,14 +3,29 @@ import { useGetGeneralLedgerQuery } from 'services/private/reports';
 import { generalLedgerReportHeadCells } from 'containers/reports/utilities/head-cells';
 import useGetGeneralLedgerData from 'containers/reports/custom-hooks/accountant/useGetGeneralLedgerData';
 import CustomReportDetailPage from '../components/CustomReportDetailPage';
+import useGetDurationInput from '../custom-hooks/common/useGetDurationInput';
+import useGetChartOfAccountTypeInput from '../custom-hooks/common/useGetChartOfAccountTypeInput';
+import { customEndDateInput, customStartDateInput } from '../utilities/filter-input-list';
+import { generalLedgerInitialValues } from '../utilities/initial-values';
+import { generalLedgerCustomInputsValidationSchema } from '../utilities/validation-schema';
 
 function GeneralLedger() {
+  const durtionInput = useGetDurationInput();
+  const chartOfAccountTypeInput = useGetChartOfAccountTypeInput();
   return (
     <CustomReportDetailPage
       reportTitle="General Ledger"
       reportHeadCells={generalLedgerReportHeadCells}
       useGetReportQuery={useGetGeneralLedgerQuery}
       useGetReportData={useGetGeneralLedgerData}
+      customReportCustomFilter={[
+        durtionInput,
+        customStartDateInput,
+        customEndDateInput,
+        chartOfAccountTypeInput,
+      ]}
+      customReportCustomerInitialValues={generalLedgerInitialValues}
+      customReportInputListValidationSchema={generalLedgerCustomInputsValidationSchema}
       options={{
         showFilter: true,
         showCompanyInfoHeader: true,
