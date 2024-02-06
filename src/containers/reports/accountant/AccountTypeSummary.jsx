2d6089b6c53ff202b24464846a2a70cbad4819a6
Arslan Ashiq <arslanashiq@beyonderissolutions.com>
2024-02-06 19:36:26 +0500

    account type summary filters

--- src/containers/reports/accountant/AccountTypeSummary.jsx
+++ src/containers/reports/accountant/AccountTypeSummary.jsx
@@ -3,14 +3,22 @@ import { useGetAccountTypeSummaryQuery } from 'services/private/reports';
 import { accountTypeSummaryReportHeadCells } from 'containers/reports/utilities/head-cells';
 import useGetAccountTypeSummaryData from 'containers/reports/custom-hooks/accountant/useGetAccountTypeSummaryData';
 import CustomReportDetailPage from '../components/CustomReportDetailPage';
+import useGetDurationInput from '../custom-hooks/common/useGetDurationInput';
+import { customEndDateInput, customStartDateInput } from '../utilities/filter-input-list';
+import { customDuration } from '../utilities/initial-values';
+import { activityLogsFilterCustomInputsValidationSchema } from '../utilities/validation-schema';
 
 function AccountTypeSummary() {
+  const durationInput = useGetDurationInput();
   return (
     <CustomReportDetailPage
       reportTitle="Account Type Summary"
       reportHeadCells={accountTypeSummaryReportHeadCells}
       useGetReportQuery={useGetAccountTypeSummaryQuery}
       useGetReportData={useGetAccountTypeSummaryData}
+      customReportCustomFilter={[durationInput, customStartDateInput, customEndDateInput]}
+      customReportCustomerInitialValues={customDuration}
+      customReportInputListValidationSchema={activityLogsFilterCustomInputsValidationSchema}
     />
   );
 }
